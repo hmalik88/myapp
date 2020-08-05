7970489253b6b04c8ba6914fa037e7459012ec3b
hmalik88 <hbmalik88@gmail.com>
2020-08-04 21:36:29 -0400

    migrated pages to bootstrap and also created leads.pug file

--- controllers/landing.js
+++ controllers/landing.js
@@ -12,13 +12,13 @@ exports.submit_lead = function(req, res, next) {
 
 exports.show_leads= function(req, res, next) {
     return models.Lead.findAll().then(leads => {
-        res.render('landing', { title: 'Express', leads: leads});
+        res.render('lead/leads', { title: 'Express', leads: leads});
     })
 }
 
 exports.show_lead = function(req, res, next) {
     return models.Lead.findOne({where: {id: req.params.lead_id}}).then(lead => {
-        res.render('lead', {lead: lead});
+        res.render('lead/lead', {lead: lead});
     })
 }
 
