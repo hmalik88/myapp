7e56bbb522a52800e1a7893a48498ebd6b1427c8
hmalik88 <hbmalik88@gmail.com>
2020-08-05 17:37:22 -0400

    implement passport local strategy using email and password fields

---
+++ passport_setup.js
@@ -0,0 +1,45 @@
+let LocalStrategy = require('passport-local').Strategy;
+
+module.exports = function(passport) {
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+    passport.deserializeUser(function(id, done) {
+        models.User.findOne({
+            where: {
+                'id': id
+            }
+        }).then(user => {
+            if (user === null) {
+                done(new Error('Wrong user id.'))
+            }
+            done(null, user);
+        })
+    });
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function(req, email, password, done) {
+        return models.User.findOne({
+            where: {
+                'email': email
+            }
+        }).then(user => {
+            if (user === null) {
+                req.flash('message', 'Incorrect credentials');
+                return done(null, false);
+            } else if (user.password == null) {
+                req.flash('message', 'You must reset your password')
+                return done(null, false);
+            } else if (!validPassword(user, password)) {
+                req.flash('message', 'Incorrect credentials')
+                return done(null, false)
+            }
+            return done(null, user)
+        }).catch(err => {
+            done(err, false);
+        })
+    }))
+
+}
